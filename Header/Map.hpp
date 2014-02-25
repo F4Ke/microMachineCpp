fd681b1179b2d95a9dc071ac7ba03ea92ca8a819
Luis <jean-louis.darmon@epitech.eu>
2014-02-25 22:51:55 +0100

    Modif by Forel_m
    Modif:
    -StartInstance.hpp
    -TimeOut.hpp
    -Power.hpp
    -Power.cpp
    -StartInstance.cpp
    
    Add:
    -Map.hpp
    -Map.cpp

---
+++ Header/Map.hpp
@@ -0,0 +1,35 @@
+#ifndef		__MAP_HH__
+# define       	__MAP_HH__
+
+#include	<string>
+#include	<iostream>
+#include	<vector>
+#include	<fstream>
+#include    "myException.hpp"
+//class		MyGame;
+
+class		Map
+{
+private :
+  std::vector<std::string> _map;
+  int x;
+  int y;
+
+public :
+
+  Map();
+  ~Map();
+
+  std::vector<std::string> parser(std::string path);
+  int	checker(std::string path);
+
+  void setMap(std::vector<std::string> &map) {_map = map;}
+
+  int getX() { return x; }
+  int getY() { return y; }
+
+  std::vector<std::string> getMap() { return _map; } // A MEDITER
+
+};
+
+#endif		// !__MAP_HH__
---
+++ Sources/Map.cpp
@@ -0,0 +1,59 @@
+#include	"Map.hpp"
+
+Map::Map()
+{
+  x = 0;
+  y = 0;
+}
+
+Map::~Map()
+{
+}
+
+int			Map::checker(std::string path)
+{
+  std::ifstream		fd(path.c_str(), std::ios::in);
+  std::string		tmp;
+  int			xlines = 0;
+  int			ylines = 0;
+  int			xcheck = 0;
+
+  if (fd)
+    {
+      std::string	str;
+      getline(fd, str);
+      xcheck = str.size();
+      while (getline(fd, str))
+	{
+	  tmp = str;
+	  xlines = tmp.size();
+	  if (xcheck != xlines)
+	    return (1);
+	  xcheck = xlines;
+	}
+      return (0);
+    }
+}
+
+std::vector<std::string> Map::parser(std::string path)
+{
+  std::ifstream fd(path.c_str(), std::ios::in);
+  std::string tmp;
+
+  if (fd)
+    {
+      std::string       str;
+      if (checker(path) == 1)
+	throw myException("Map Error.\n");
+      while (getline(fd, str))
+	{
+	  _map.push_back(str);
+	  y++;
+	}
+      tmp = str;
+    }
+  x = tmp.size();
+  return _map;
+}
+
+
