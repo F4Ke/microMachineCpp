903dbdc9fc47a9d5a6bbd36b8c216cc2fd130d23
F4Ke <matthieu.forel@epitech.eu>
2014-03-10 14:40:50 +0100

    micro

---
+++ MicroMachineMofos/MicroMachine.cpp
@@ -0,0 +1,20 @@
+#include    "MicroMachine.hpp"
+#include    "StartInstance.hpp"
+
+//Constructeur + Destructeur
+
+MicroMachine::MicroMachine() {
+}
+
+MicroMachine::~MicroMachine() {
+}
+
+//Methodes
+
+void    MicroMachine::setOnMap()
+{
+    
+ 
+	return ;
+}
+
---
+++ MicroMachineMofos/MicroMachine.hpp
@@ -0,0 +1,23 @@
+#ifndef __MICROMACHINE__
+#define __MICROMACHINE__
+
+#include    <iostream>
+#include    <vector>
+#include    <string>
+
+#include    "Map.hpp"
+
+
+class       MicroMachine 
+{
+
+public:
+    
+	MicroMachine();
+    ~MicroMachine();
+    
+    
+    void   setOnMap();
+};
+
+#endif /* defined(__MICROMACHINE__) */
